a44953f8bc5f2b6838f2cbfaf549fb677885b8b0
Bareera-Maheen <168240951+Bareera-Maheen@users.noreply.github.com>
2024-11-29 04:19:59 +0500

    Update page.tsx
    j

--- src/app/offers/page.tsx
+++ src/app/offers/page.tsx
@@ -8,19 +8,19 @@ const Offers = () => {
         <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
           <div className="p-6 transition-transform transform bg-white rounded-lg shadow-md hover:scale-105">
             <h3  className="text-2xl font-bold">DAILY PACK</h3>
-            <p className="text-xl font-semibold">25,00 rupees</p>
+            <p className="text-xl font-semibold">2500 rupees</p>
             <button className="px-4 py-2 mt-4 text-white transition bg-blue-600 rounded hover:bg-blue-700"><a href="/order" className="text-black-700 hover:text-blue-900">Order</a></button><br />
             <button className="px-4 py-2 mt-4 text-white transition bg-blue-600 rounded hover:bg-blue-700">❤️ Love</button>
           </div>
           <div   className="p-6 transition-transform transform bg-white rounded-lg shadow-md hover:scale-105">
             <h3 className="text-2xl font-bold"> PARTY PACK</h3>
-            <p className="text-xl font-semibold">5,000 rupees</p>
+            <p className="text-xl font-semibold">5000 rupees</p>
             <button className="px-4 py-2 mt-4 text-white transition bg-blue-600 rounded hover:bg-blue-700"><a href="/order" className="text-black-700 hover:text-blue-900">Order</a></button><br />
             <button className="px-4 py-2 mt-4 text-white transition bg-blue-600 rounded hover:bg-blue-700">❤️ Love</button>
           </div>
           <div  className="p-6 transition-transform transform bg-white rounded-lg shadow-md hover:scale-105">
             <h3 className="text-2xl font-bold">SUPER DUPER PACK</h3>
-            <p className="text-xl font-semibold">7,000 rupees</p>
+            <p className="text-xl font-semibold">7000 rupees</p>
             <button className="px-4 py-2 mt-4 text-white transition bg-blue-600 rounded hover:bg-blue-700"><a href="/order" className="text-black-700 hover:text-blue-900">Order</a></button><br />
             <button className="px-4 py-2 mt-4 text-white transition bg-blue-600 rounded hover:bg-blue-700">❤️ Love</button>
           </div>
